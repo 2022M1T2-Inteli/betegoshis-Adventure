6c572ea75ffed1b9996443b5d270b46c33cfc302
joaoalca <joao.alcaraz@sou.inteli.edu.br>
2022-03-17 14:58:02 -0300

    menu do jogo ~ joao

---
+++ documentos/Menu do jogo(joao)/MainMenu.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+
+func _on_Button1_pressed():
+	get_tree().change_scene("res://FirstScene.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Node2D.tscn")
+
+
+func _on_Button3_pressed():
+	get_tree().quit()
