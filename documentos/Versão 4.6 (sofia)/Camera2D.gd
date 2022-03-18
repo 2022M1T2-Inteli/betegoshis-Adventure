b073bead35d6edb61f52e2be0d11a6e7b9fdf3cb
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-03-18 08:12:38 -0300

    Versão 4.7 do jogo

--- documentos/Versão 4.6 (sofia)/Camera2D.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-func _ready():
-	make_current()
-#	$Controles.modulate.a = 0.45
-#	$Config/iPad/apps/MapaB/MapaI.visible = false
-#	$Config/iPad.visible = false
-#	$Fundo/AnimationPlayer.play("gif fundo")
