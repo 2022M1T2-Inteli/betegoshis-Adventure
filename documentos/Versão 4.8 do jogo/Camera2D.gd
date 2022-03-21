4dea6ee78af9ae8c354cd597d2c7be68cdee4109
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-03-21 15:53:57 -0300

    Versão 4.8
    Algumas mudancinhas

---
+++ documentos/Versão 4.8 do jogo/Camera2D.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+func _ready():
+	make_current()
+	$Controles/Controlesorg.modulate.a = 0.1
+#	$Controles.modulate.a = 0.45
+#	$Config/iPad/apps/MapaB/MapaI.visible = false
+#	$Config/iPad.visible = false
+#	$Fundo/AnimationPlayer.play("gif fundo")
---
+++ documentos/Versão 4.8 do jogo/Config.gd
@@ -0,0 +1,33 @@
+extends Button # Script do Botão AbrirCelular
+
+
+func _on_MapaB_pressed(): #botão do app "Mapa"
+	$CelularPanel/apps/MapaB/MapaI.visible = true #aparece o mapa
+	$CelularPanel/apps/MapaB/CloseMAPButton.visible = true #aparece o botao de fechar
+
+func _on_CloseMAPButton_pressed(): #quando aperta no "x"
+	$CelularPanel/apps/MapaB/MapaI.visible = false #o mapa desaparece
+	$CelularPanel/apps/MapaB/CloseMAPButton.visible = false #o botao de fechar também desaparece
+
+func _process(_delta):
+	if Global.objetivo1 == true:
+		$CelularPanel/Tasks/Objetivo2.visible = true
+		$CelularPanel/Tasks/Objetivo1.modulate.r = 0
+		$CelularPanel/Tasks/Objetivo1.modulate.b = 0
+	if Global.objetivo2 == true:
+		$CelularPanel/Tasks/Objetivo3.visible = true
+		$CelularPanel/Tasks/Objetivo2.modulate.r = 0
+		$CelularPanel/Tasks/Objetivo2.modulate.b = 0
+	if Global.objetivo3 == true:
+		$Notificacao.visible = true
+		$CelularPanel/Tasks/Objetivo4.visible = true
+		$CelularPanel/Tasks/Objetivo3.modulate.r = 0
+		$CelularPanel/Tasks/Objetivo3.modulate.b = 0
+	if Global.objetivo4 == true:
+		$CelularPanel/Tasks/Objetivo5.visible = true
+		$CelularPanel/Tasks/Objetivo4.modulate.r = 0
+		$CelularPanel/Tasks/Objetivo4.modulate.b = 0
+	if Global.objetivo5 == true:
+		$CelularPanel/Tasks/Objetivo6.visible = true
+		$CelularPanel/Tasks/Objetivo5.modulate.r = 0
+		$CelularPanel/Tasks/Objetivo5.modulate.b = 0
