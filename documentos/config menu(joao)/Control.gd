c60aedb44000693a5c304e47c83c770c545d1948
joaoalca <joao.alcaraz@sou.inteli.edu.br>
2022-03-21 15:01:11 -0300

    oi

---
+++ documentos/config menu(joao)/Control.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var soundoff_buttons = preload("res://images/buttons/audioOff.png")
+var soundon_buttons = preload("res://images/buttons/audioOn.png")
+var musicon_buttons = preload("res://images/buttons/musicOn.png")
+var musicoff_buttons = preload("res://images/buttons/musicOff.png")
+var sound = true
+var music = true
+
+func _on_TextureButton_pressed():
+	if sound:
+		sound = false
+		$TextureButton.texture_normal = soundoff_buttons
+	else:
+		sound = true
+		$TextureButton.texture_normal = soundon_buttons
+
+func _on_TextureButton2_pressed():
+	if music:
+		music = false
+		$TextureButton2.texture_normal = musicoff_buttons
+	else:
+		music = true
+		$TextureButton2.texture_normal = musicon_buttons
