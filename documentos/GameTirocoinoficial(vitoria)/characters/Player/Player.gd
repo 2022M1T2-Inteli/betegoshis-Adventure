c37edcf53573c7a56f842d53af46d317f733711c
R0drigu3svi <99213209+R0drigu3svi@users.noreply.github.com>
2022-03-25 09:23:40 -0300

    Gametirocoinofc
    joguinho da vivi

--- documentos/GameTirocoinoficial(vitoria)/characters/Player/Player.gd
+++ documentos/GameTirocoinoficial(vitoria)/characters/Player/Player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 class_name Player 
 
 signal spawn_laser(location)
@@ -19,6 +19,7 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("shoot"):
 		shoot_laser()
+	move_and_slide(input_vector)
 	
 func take_damage(damage):
 	print(hp)
@@ -29,8 +30,11 @@ func take_damage(damage):
 		get_tree().paused = true 
 		
 func _on_Player_area_entered(area):
+	print(area)
 	if area.is_in_group("enemies"):
+		print('foi')
 		area.take_damage(1)
+		take_damage(1)
 		
 func shoot_laser():
 	emit_signal("spawn_laser", muzzle.global_position)
